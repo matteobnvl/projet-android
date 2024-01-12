845b75b1d6558470ed0e21353d26674ff8ebf112
matteobnvl <matteobonneval19@gmail.com>
2024-01-12 08:50:36 +0100

    init

---
+++ app/src/main/java/fr/matteo/projetandroid/TabBarActivity.kt
@@ -0,0 +1,32 @@
+package fr.matteo.projetandroid
+
+import android.os.Bundle
+import android.widget.TextView
+
+class TabBarActivity : BaseActivity() {
+
+    //val tab1Fragment= Tab1Fragment.newInstance("","")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_tabbar)
+        //showBack()
+        //setHeaderTitle(getString(R.string.txtTabbar))
+
+        //val tab1= findViewById<TextView>(R.id.textViewTab1)
+
+        //showTab1()
+
+        //tab1.setOnClickListener {
+            //showTab1()
+        //}
+    }
+
+    //fun showTab1(){
+        //val frManager=supportFragmentManager
+        //val fragmentTra= frManager.beginTransaction()
+        //fragmentTra.addToBackStack("Tab1")
+        //fragmentTra.replace(R.id.layoutContent,tab1Fragment)
+        //fragmentTra.commit()
+    //}
+}
